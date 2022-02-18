915c4ff7919c2eaa94fe44134e0fd9db756353e1
KHUNBIRD <birdpakornpong@gmail.com>
2022-02-18 22:35:35 +0700

    feat/update

---
+++ letvar.js
@@ -0,0 +1,12 @@
+function handlerSomething() {
+    let bird = "pakornpong"
+    if (bird === "pakornpong") {
+        var pang = "nan"
+        let birds = "bird"
+    }
+
+    console.log('pang => var', pang)
+    console.log('birds => let', birds) // ไม่เจอ
+}
+
+handlerSomething()
