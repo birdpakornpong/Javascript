f1f6a1d6e68c3fc2085a5fee279dc3684c0ec05f
KHUNBIRD <birdpakornpong@gmail.com>
2022-02-18 22:52:40 +0700

    promise async await

--- promise-asyncawait.js
+++ promise-asyncawait.js
@@ -1,4 +1,40 @@
 // Promise
+function getValue(age) {
+    return Promise.resolve(`Hello I am Pakornpong age ${age} year old`); // Promise.resolve แก้ไข ไม่ return promise ทำให้ error ".then is not a function"
+}
 
+function promiseFunction(number) {
+    getValue(number)
+    .then(result => {
+        console.log('result', result)
+        return `bird`
+    })
+    .then(response => console.log("response",response))
+    .catch(err => {
+        console.log('err',err)
+    })
+    // ไม่สามารถ .then หลัง catch ได้
+}
+
+promiseFunction(25)
 
 // Async Await
+
+async function asyncFunction(number) {
+    try {
+        const data = await getValue(number)
+        console.log('data', data)
+    } catch(err) {
+        console.log('err', err)
+    }
+
+    try {
+        const data = await getValue(number)
+        console.log('data2', data)
+    } catch(err) {
+        console.log('err', err)
+    }
+  
+}
+
+asyncFunction(26)
